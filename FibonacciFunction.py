2168984b3deeea8b04674a680fc303411411be5e
agbruneau <agbruneau@gmail.com>
2019-05-30 06:23:56 -0400

    corrections de bugs sur les limites des paramètres entiers

--- FibonacciFunction.py
+++ FibonacciFunction.py
@@ -2,7 +2,7 @@
 import timeit
 
 def fib_doubling(n):
-    if n < 2:
+    if (n < 2) or (n > 100000000):
         return n
     """ Calculate the Nth Fibonacci number using the doubling method. """
     return _fib_doubling(n)[0]
@@ -25,6 +25,15 @@ def _fib_doubling(n):
 # Main Python
 if __name__ == "__main__":
 
+    # Tests parameters limites
+    print("fib_doub             -2 :\t",timeit.timeit('fib_doubling(-2)', globals=globals(), number=1))
+    print("fib_doub   100 000 001 :\t", timeit.timeit('fib_doubling(100000001)', globals=globals(), number=1))
+
+    # Compute parameters
+    print("fib_doub             1 :\t", timeit.timeit('fib_doubling(1)', globals=globals(), number=1))
+    print("fib_doub            10 :\t", timeit.timeit('fib_doubling(10)', globals=globals(), number=1))
+    print("fib_doub           100 :\t", timeit.timeit('fib_doubling(100)', globals=globals(), number=1))
+    print("fib_doub           100 :\t", timeit.timeit('fib_doubling(100)', globals=globals(), number=1))
     print("fib_doub           100 :\t", timeit.timeit('fib_doubling(100)', globals=globals(), number=1))
     print("fib_doub         1 000 :\t", timeit.timeit('fib_doubling(1000)', globals=globals(), number=1))
     print("fib_doub        10 000 :\t", timeit.timeit('fib_doubling(10000)', globals=globals(), number=1))
