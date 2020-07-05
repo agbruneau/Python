e8db8108e8cc4884ab2e4d1d1755922f4814021f
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2020-07-04 20:48:49 -0400

    update SH files

--- AGB/pypyF.sh
+++ AGB/pypyF.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-echo "#### Début FibonacciFuntion à 8 X 100 millions ####"
-pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & 
-echo "#### Fin FibonacciFunction à 8 x 100 millions ####"
+echo "#### Début FibonacciFuntion à 12 X 100 millions ####"
+pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & 
+echo "#### Fin FibonacciFunction à 12 x 100 millions ####"
 
--- AGB/pythonF.sh
+++ AGB/pythonF.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 echo "#### Début FibonacciFuntion à 8 X 100 millions ####"
-python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & 
+python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py & python3 FibonacciFunction.py &
 echo "#### Fin FibonacciFunction à 8 x 100 millions ####"
 
