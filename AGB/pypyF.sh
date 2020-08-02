8fb3de3944521298f90b836e6744020acdd41c7d
André-Guy Bruneau <agbruneau@gmail.com>
2020-08-02 11:40:14 -0400

    add 12 core call on .sh file

--- AGB/pypyF.sh
+++ AGB/pypyF.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 echo "#### Début FibonacciFuntion à 8 X 100 millions ####"
-pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & 
+pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & 
 echo "#### Fin FibonacciFunction à 8 x 100 millions ####"
 
