9b0b05b9f15df88d82fb1b99406024ea07fcb930
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2020-08-15 16:12:27 -0400

    Delete pypy.sh

--- AGB/pypyF.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-echo "#### Début FibonacciFuntion à 8 X 100 millions ####"
-pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & pypy3 FibonacciFunction.py & 
-echo "#### Fin FibonacciFunction à 8 x 100 millions ####"
-
